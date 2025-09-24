27c3eac205b61287bb1c384f04ef226b158bba5b
Mahendra <varmamahendra14@gmail.com>
2025-09-24 12:04:37 -0600

    teo pointer problems

---
+++ Two pointer/two_sum.py
@@ -0,0 +1,19 @@
+# leetcode-167
+
+from typing import List
+
+
+class Solution:
+    # solving to learn two pointer technique
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        j = len(numbers) - 1
+        i = 0
+        while i < j:
+            s = numbers[j] + numbers[i]
+            if s == target:
+                return [i+1,j+1]
+            elif s < target:
+                i+=1
+            else:
+                j-=1
+        return []
