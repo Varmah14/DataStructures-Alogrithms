011c21f85e8885275a0d58c562d7fffa0c892a9b
Mahendra <varmamahendra14@gmail.com>
2026-02-22 18:20:11 -0700

    Heaps

---
+++ syntax_practice.py
@@ -0,0 +1,29 @@
+arr = []
+arr.append(1)
+arr.pop()
+
+dict = {}
+dict[0] = 1
+dict.get(2, 0)
+for key, value in dict.items():
+    pass
+
+# set
+s = set()
+s.add(1)
+s.remove(1)
+
+# counter
+
+
+# stack
+
+stack = []
+stack.append(1)
+stack.pop()
+
+from collections import deque
+
+queue = deque()
+queue.append(1)
+queue.popleft()
